6a7f03a34f85337d39b19a45a84c615c4f66a712
SilverIceKey <z516798599@gmail.com>
2024-07-15 10:33:19 +0800

    移除内联函数，降版本

--- SIKNotification/src/main/java/com/sik/notification/SIKNotificationUtils.kt
+++ SIKNotification/src/main/java/com/sik/notification/SIKNotificationUtils.kt
@@ -231,49 +231,6 @@ object SIKNotificationUtils {
     }
 
     /**
-     * 显示通知
-     * @param context 上下文
-     * @param title 通知标题
-     * @param content 通知内容
-     * @param icon 通知图标资源 ID（可选）
-     * @return Boolean 如果通知通道不存在则返回false
-     */
-    @SuppressLint("MissingPermission")
-    fun <T : SIKNotificationChannelConfig> showNotification(
-        context: Context, config: T, title: String, content: String, icon: Int = appIcon
-    ): Boolean {
-        if (config.channelId.isNullOrEmpty()) {
-            return false
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            if (notificationManager == null) {
-                throw NullPointerException("请先调用SIKNotificationUtils.init(context:Context)进行初始化")
-            }
-            if (notificationManager?.getNotificationChannel(config.channelId) == null) {
-                createOrUpdateNotificationChannel(config)
-            }
-        }
-
-        val notificationId = (notificationIdMap[config::class.java.simpleName] ?: 0) + 1
-
-        val builder = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationCompat.Builder(context, config.channelId)
-        } else {
-            NotificationCompat.Builder(context)
-        }
-
-        builder.setSmallIcon(icon).setContentTitle(title).setContentText(content)
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT).setAutoCancel(true)
-
-        with(NotificationManagerCompat.from(context)) {
-            notify(notificationId, builder.build())
-        }
-
-        notificationIdMap[config::class.java.simpleName] = notificationId
-        return true
-    }
-
-    /**
      * 获取应用的默认图标
      * @param context 上下文
      * @return 应用图标资源 ID
